ec9adc8710abf60d0f622bb296d781beebbcb907
AtulPattewar <106136893+AtulPattewar@users.noreply.github.com>
2022-10-12 17:12:26 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"practice/user"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func hello(c *fiber.Ctx) error {
+	return c.SendString("Welcome to go lang")
+}
+func Routers(app *fiber.App) {
+	app.Get("/users", user.GetUsers)
+	app.Get("/user/:id", user.GetUser)
+	app.Post("/user", user.SaveUser)
+	app.Delete("/user/:id", user.DeleteUser)
+	app.Put("/user", user.UpdateUser)
+
+}
+
+func main() {
+	user.InitialMigration()
+	app := fiber.New()
+	app.Get("/", hello)
+	Routers(app)
+	app.Listen(":3000")
+}
